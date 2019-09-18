b6c50180462a4e549839869eb68cb88205e5a78d
STIV3NS <jacekbizub@gmail.com>
2019-09-18 18:35:52 +0200

    Optimize Village.distance(...)

--- src/main/java/twao/villages/Village.kt
+++ src/main/java/twao/villages/Village.kt
@@ -23,9 +23,9 @@ open class Village(open val x: Int, open val y: Int) {
         = "$x|$y"
 
     companion object {
-        infix fun Int.`**`(exponent: Int): Int = toDouble().pow(exponent).toInt()
+        fun Int.squared(): Int = this * this
 
         @JvmStatic fun distance(v1: Village, v2: Village): Int
-            = ((v1.x - v2.x) `**` 2) + ((v1.y - v2.y) `**` 2)
+            = (v1.x - v2.x).squared() + (v1.y - v2.y).squared()
     }
 }
