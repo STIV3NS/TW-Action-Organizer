13676f174b7dc317163bc03f63cb05cdc698394d
STIV3NS <jacekbizub@gmail.com>
2020-08-27 19:31:37 +0200

    Fix deadlock

--- src/main/kotlin/io/github/stiv3ns/twactionorganizer/logging/Logger.kt
+++ src/main/kotlin/io/github/stiv3ns/twactionorganizer/logging/Logger.kt
@@ -1,16 +1,17 @@
 package io.github.stiv3ns.twactionorganizer.logging
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.runBlocking
 import logActor
 import kotlin.reflect.KClass
 
 @ObsoleteCoroutinesApi
-object Logger {
-    private val log: SendChannel<LogMessage> by lazy {
-        runBlocking { logActor() }
-    }
+object Logger : CoroutineScope {
+    override val coroutineContext = Dispatchers.Default
+
+    private val log = logActor()
 
     suspend fun info(text: String) {
         log.send(Info(text))
