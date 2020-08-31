8a099704ce83078943ff24253cfb237894ff2fb0
STIV3NS <jacekbizub@gmail.com>
2020-08-31 18:19:47 +0200

    Improve AssignerType documentation

--- src/main/kotlin/io/github/stiv3ns/twactionorganizer/core/assigners/AssignerType.kt
+++ src/main/kotlin/io/github/stiv3ns/twactionorganizer/core/assigners/AssignerType.kt
@@ -1,54 +1,60 @@
 package io.github.stiv3ns.twactionorganizer.core.assigners
 
 /**
- * Determines assigner behavior
- *
- * ---------------------------------------------------------------------------------------------------------------
- * [RAM]:
- * output assigment: Concrete attack without noble
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with nearest target.
- *---------------------------------------------------------------------------------------------------------------
- * [REVERSED_RAM]:
- * output assigment: Concrete attack without noble
- * behavior: Pick an TARGET village that is closest to the referencePoint and link it with nearest ally village.
- *---------------------------------------------------------------------------------------------------------------
- * [RANDOMIZED_RAM]:
- * output assigment: Concrete attack without noble
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with RANDOM target.
- *---------------------------------------------------------------------------------------------------------------
- * [FAKE_RAM]:
- * output assigment: Fake attack without noble
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with nearest target.
- *---------------------------------------------------------------------------------------------------------------
- * [RANDOMIZED_FAKE_RAM]:
- * output assigment: Fake attack without noble
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with RANDOM target.
- *---------------------------------------------------------------------------------------------------------------
- * [NOBLE]:
- * output assigment: Concrete attack with noble
- * behavior: Pick an TARGET village that is closest to the relativityPoint and link it with nearest ally village.
- *---------------------------------------------------------------------------------------------------------------
- * [FAKE_NOBKE]:
- * output assigment: Fake attack with noble
- * behavior: Pick an TARGET village that is closest to the relativityPoint and link it with nearest ally village.
- * ---------------------------------------------------------------------------------------------------------------
- * [DEMOLITION]:
- * output assigment: Demolition attack
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with nearest target.
- *---------------------------------------------------------------------------------------------------------------
- * [RANDOMIZED_DEMOLITION]:
- * output assigment: Demolition attack
- * behavior: Pick an ALLY village that is the farthest from referencePoint and link it with RANDOM target.
- *---------------------------------------------------------------------------------------------------------------
+ * Determines assigner behaviour
  */
 enum class AssignerType {
+    /**
+     * output assigment: Concrete attack without noble.
+     * behaviour: Pick an ALLY village that is the farthest from the referencePoint and link it with nearest target.
+     */
     RAM,
+
+    /**
+     * output assigment: Concrete attack without noble.
+     * behaviour: Pick an TARGET village that is the closest to the referencePoint and link it with nearest ally village.
+     */
     REVERSED_RAM,
+
+    /**
+     * output assigment: Concrete attack without noble.
+     * behaviour: Pick an ALLY village that is the farthest from the referencePoint and link it with RANDOM target.
+     */
     RANDOMIZED_RAM,
+
+    /**
+     * output assigment: Fake attack without noble.
+     * behaviour: Pick an ALLY village that is the farthest from the referencePoint and link it with nearest target.
+     */
     FAKE_RAM,
+
+    /**
+     * output assigment: Fake attack without noble.
+     * behaviour: Pick an ALLY village that is the farthest from the referencePoint and link it with RANDOM target.
+     */
     RANDOMIZED_FAKE_RAM,
+
+    /**
+     * output assigment: Concrete attack with noble.
+     * behaviour: Pick an TARGET village that is the closest to the referencePoint and link it with nearest ally village.
+     */
     NOBLE,
+
+    /**
+     * output assigment: Fake attack with noble.
+     * behaviour: Pick an TARGET village that is the closest to the referencePoint and link it with nearest ally village.
+     */
     FAKE_NOBLE,
+
+    /**
+     * output assigment: Demolition attack.
+     * behaviour: Pick an ALLY village that is the farthest from referencePoint and link it with nearest target.
+     */
     DEMOLITION,
+
+    /**
+     * output assigment: Demolition attack.
+     * behaviour: Pick an ALLY village that is the farthest from referencePoint and link it with RANDOM target.
+     */
     RANDOMIZED_DEMOLITION
 }
