4fe9c5e0519dfeceab275ea029638cc7cc8a6e86
STIV3NS <jacekbizub@gmail.com>
2020-08-31 18:19:33 +0200

    Remove unused import

--- src/main/kotlin/io/github/stiv3ns/twactionorganizer/core/assigners/StandardDemolitionAssigner.kt
+++ src/main/kotlin/io/github/stiv3ns/twactionorganizer/core/assigners/StandardDemolitionAssigner.kt
@@ -1,6 +1,5 @@
 package io.github.stiv3ns.twactionorganizer.core.assigners
 
-import io.github.stiv3ns.twactionorganizer.core.Player
 import io.github.stiv3ns.twactionorganizer.core.Resources
 import io.github.stiv3ns.twactionorganizer.core.villages.TargetVillage
 import io.github.stiv3ns.twactionorganizer.core.villages.Village
